5f1be5cc2e9d012b7b9bf7c7c407455453d78dec
Amar B <amar@amarkbo.com>
2014-07-25 19:24:15 -0400

    Content and style updates

--- build.js
+++ build.js
@@ -6,6 +6,7 @@ var Metalsmith = require('metalsmith');
 var sass = require('metalsmith-sass');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
+var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
 var metaobject = require('metalsmith-metaobject');
 
@@ -13,9 +14,14 @@ var settings = require('./settings_local.json');
 
 var metalsmith_src = Metalsmith(__dirname)
 
-    .use(metaobject(settings))
+metalsmith_src.use(metaobject(settings))
 
-    //.use(markdown())
+    .use(collections({
+        works: {
+            pattern: 'works/*.html',
+            sortBy: 'date' 
+        }
+    }))
 
     .use(templates({
         engine: 'swig',
@@ -27,11 +33,6 @@ var metalsmith_src = Metalsmith(__dirname)
         imagePath: '/'
     }))
 
-    .use(permalinks({
-        pattern: ':title',
-        relative: false
-    }))
-
     .build(function(err){
         if (err) throw err;
 
@@ -41,31 +42,3 @@ var metalsmith_src = Metalsmith(__dirname)
             console.log(stderr);
         });
     });
-
-/**
- * Concat plugin.
- *
- * @param {Object} files
- * @param {Metalsmith} metalsmith
- * @param {Function} done
- */
-
-/*
-function concat(files, metalsmith, done){
-  var css = '';
-
-  for (var file in files) {
-    if ('.css' != extname(file)) continue;
-    css += files[file].contents.toString();
-    delete files[file];
-  }
-
-  css = myth(css);
-
-  files['index.css'] = {
-    contents: new Buffer(css)
-  };
-
-  done();
-}
-*/
